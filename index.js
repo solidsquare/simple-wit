6107c58a3f25116743e8d3c09e191e4fa7a603a0
Brendan Donovan <brendan.donovan@frogdesign.com>
2018-01-23 13:37:57 +0100

    Added Request node library and API call to Open Weather Map for the weather at a location returned from Wit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Wit } = require('node-wit');
+const request = require('request');
 
 const client = new Wit({
 	accessToken: 'ServerAccessToken'
@@ -9,6 +10,23 @@ client
 	.then(data => {
 		//console.log('Where are we checking: ' + data.entities.location[0].value);
 		console.log(data.entities.intent);
-
+		if(data.entities.intent[0].value === 'weather'){
+			if(data.entities.location[0].value) {
+				let apiKey = 'OPENWEATHERMAPAPIKEY';
+				let city = data.entities.location[0].value;
+				let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+				console.log('The weather in ' + city + ' is ');
+				request(url, function (err, response, body) {
+				  if(err){
+				    console.log('error:', error);
+				  } else {
+						let weather = JSON.parse(body);
+				    console.log(weather.weather[0].main);
+				  }
+				});
+			}
+		}else {
+			console.log('Do not compute');
+		}
 	})
 	.catch(console.error);
